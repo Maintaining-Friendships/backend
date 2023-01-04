06539e147610a7c39016374307b29336b94c6d0f
Henrymarks1 <henryesmarks@gmail.com>
2023-01-04 14:05:54 -0800

    Working on Stimulus controller
    Figure out what it means by and Creating an Index

--- dist/src/controllers/stimulusController.js
+++ dist/src/controllers/stimulusController.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,18 +33,24 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const responses_1 = require("../middleware/responses");
+const admin = __importStar(require("firebase-admin"));
 exports.default = {
     openEndedQuestion: function (req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             //Function that returns an open ended question for people to talk about
-            let listOfQuestions = [
-                "What was the most interesting thing you did today?", "Who is your hero?", "What would you change about yourself if you could?"
-            ];
-            let random_idx = Math.floor(Math.random() * listOfQuestions.length);
-            let question = listOfQuestions[random_idx];
-            return (0, responses_1.successResponse)(res, {
-                question: question
-            });
+            const collection = admin.firestore().collection("/stimulus");
+            const random = Math.random();
+            let randomDoc = collection.orderBy(admin.firestore.FieldPath.documentId(), random > 0.5 ? "asc" : "desc");
+            randomDoc = randomDoc.limit(1);
+            const snapshot = yield randomDoc.get();
+            const documents = snapshot.docs;
+            if (documents.length > 0) {
+                const randomDocument = documents[0];
+                const documentData = randomDocument.data();
+                return (0, responses_1.successResponse)(res, {
+                    documentData,
+                });
+            }
         });
-    }
+    },
 };
--- src/controllers/stimulusController.ts
+++ src/controllers/stimulusController.ts
@@ -1,19 +1,30 @@
-import { successResponse } from "../middleware/responses"
-import { Request, Response } from 'express'
-
+import { successResponse } from "../middleware/responses";
+import { Request, Response } from "express";
+import * as admin from "firebase-admin";
 
 export default {
-    openEndedQuestion: async function (req: Request, res: Response) {
-        //Function that returns an open ended question for people to talk about
-        let listOfQuestions: string[]  = [
-            "What was the most interesting thing you did today?", "Who is your hero?", "What would you change about yourself if you could?"
-        ]
+  openEndedQuestion: async function (req: Request, res: Response) {
+    //Function that returns an open ended question for people to talk about
+    const collection = admin.firestore().collection("/stimulus");
 
-        let random_idx = Math.floor(Math.random() * listOfQuestions.length)
-        let question: String = listOfQuestions[random_idx]
-        
-        return successResponse(res, {
-            question: question
-        })
+    const random = Math.random();
+
+    let randomDoc = collection.orderBy(
+      admin.firestore.FieldPath.documentId(),
+      random > 0.5 ? "asc" : "desc"
+    );
+
+    randomDoc = randomDoc.limit(1);
+
+    const snapshot = await randomDoc.get();
+    const documents = snapshot.docs;
+
+    if (documents.length > 0) {
+      const randomDocument = documents[0];
+      const documentData = randomDocument.data();
+      return successResponse(res, {
+        documentData,
+      });
     }
-}
+  },
+};
